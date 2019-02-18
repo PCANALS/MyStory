3505266106a8d4d03fff5783fdd5f1dc013e4a95
Pilar <pilarcanalsdros@gmail.com>
2019-02-18 17:35:58 +0100

    repeat working

--- mystory.R
+++ mystory.R
@@ -1,6 +1,6 @@
 rm(list = ls())
 
-pacman::p_load(magick, networkD3)
+pacman::p_load(magick, networkD3, pdftools)
 
 
 #Live
@@ -17,98 +17,43 @@ life<-image_animate(c(bcn, trip, snow, playa, nieve, lea),
 
 #Work
 
-cv2 <- image_read_pdf("PilarCanals.pdf")%>%image_scale("x1000")
+cv2 <- image_read_pdf("https://pcanals.github.io/pics/PilarCanals.pdf")%>%image_scale("x1000")
 
-#projects
+#Projects
 
 git<-image_annotate(image_read("https://pcanals.github.io/pics/GIT.png" )%>%image_scale("x400"), 
                     "   go to github.com/PCANALS  ", size = 30, color = "white", boxcolor = "green2",
                     degrees = 10, location = "+50+280")
-
-##Decisions
 
 ## Are u sure
 what<-image_read("https://pcanals.github.io/pics/what.gif")
 yeah<-image_read("https://pcanals.github.io/pics/yeah.gif")
 sad<-image_read("https://pcanals.github.io/pics/sad.gif")
 
-#first questions
+#first question
 
 a<-as.numeric(menu(c("Yes", "No", "Maybe"), title="Do u wanna know my story?", graphics = TRUE))
 
-
-while((a == 1|c==1){
-    b<-as.numeric(menu(c("Life", "Work", "Projects"), title="What do u want know?", graphics = TRUE))
-      if (b==1){ print(life); print("Hi, I'm Pilar and I live in Barcelona, I love travelling,the snow and Lea, my puppy")}
-        else if (b==2) {print(cv2); print("Check my cv on  www.linkedin.com/in/pilarcanals")}
-        else if (b==3) {print(git); print("See my projects on Github https://github.com/PCANALS")}
-} else {
+repeat{
+  if(a == 1){
+    b<-as.numeric(menu(c("Life", "Work", "Projects", "End Process"), title="What do u want know?", graphics = TRUE))
+    
+    if (b==1){ print(life); print("Hi, I'm Pilar and I live in Barcelona, I love travelling,the snow and Lea, my puppy")}
+    else if (b==2) {print(cv2); print("Check my cv on  www.linkedin.com/in/pilarcanals")}
+    else if (b==3) {print(git); print("See my projects on Github https://github.com/PCANALS")}
+    else if (b==4) { print("Thanks for fun the code!"); break;}
+    
+  }
+  
+  else if(a!=1) {
     print (what);
     c<-as.numeric(menu(c("no", "yes"), title="Are u sure?", graphics = TRUE ))
-      if (c==1) {print(yeah); print("Yeii!, then let's back to the beginning")
-        }
-      else if (c==2) {break}
-        }
-)
-
-
-
-
-print(sad); print("OK,  see you soon then.")
-
-# if (a == 1){
-#   b<-as.numeric(menu(c("Life", "Work", "Projects"), title="What do u want know?", graphics = TRUE))
-#   if (b==1){ print(life); 
-#     print("Hi, I'm Pilar and I live in Barcelona, I love travelling,the snow and Lea, my puppy")}
-#   else if (b==2) {print(cv2);  
-#     print("Check my cv on  www.linkedin.com/in/pilarcanals")}
-#   else if (b==3) {print(git);  
-#     print("See my projects on Github https://github.com/PCANALS")}
-# } else {
-#   print (what);
-#   c<-as.numeric(menu(c("no", "yes"), title="Are u sure?", graphics = TRUE ))
-#   if (c==1) {print(yeah); 
-#     print("Yeii!, then let's back to the beginning")
-#   }
-#   else if (c==2) {print(sad); 
-#     print("OK,  see you soon then.")
-#   }
-# }
-
-
-
-
-
-
-
-
-
-x <- 5
-if (x < 0) {
-  print("Negative number")
-} else if (x > 0) {
-  print("Positive number")
-} else
-  print("Zero")
-
-
-
-
-
-
-
-
-
-
-# print(really)
-#image_browse(what)
-# 
-# 
-# 
-# data(MisLinks, MisNodes)
-# forceNetwork(Links = MisLinks, Nodes = MisNodes, Source = "source",
-#              Target = "target", Value = "value", NodeID = "name",
-#              Group = "group", opacity = 0.8)
-
-
+    if (c==1) {print(yeah); print("Yeii!, then let's back to the beginning")
+      a<-1
+    }
+    else if (c==2) { print(sad); print("OK,  see you soon then...");
+      break;
+    }
+  }
+}
 
